e8451dda8b7512d1064e70fc57909e4a0f5a42cb
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-08-11 11:36:05 +0200

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -35,13 +35,12 @@ def banner():
 ╚═╝     ╚══════╝╚═╝  ╚══╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝{color.fin}""")
 def version():
  texto ="""
- |==========================================================|
- | Script by              :    #FENRIR-00                   |
- | Version                :    Version  1.2                 |
- | Follow me on Github    :    https://github.com/Fenrir-00 |
- | Contact me on Telegram :    @Ritorito1990                |
- ============================================================    """
- lol_py(texto)
+ |=======================================================|
+ | Script by              : #FENRIR-00                   |
+ | Version                : Version  1.2                 |
+ | Follow me on Github    : https://github.com/Fenrir-00 |
+ | Contact me on Telegram : @Ritorito1990                |
+ ========================================================= """ lol_py(texto)
  print()
 
 def carga():
