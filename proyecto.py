276b02e40895ed5d56bfefe78311a8bea2247476
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-09-11 00:06:41 +0200

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -261,12 +261,12 @@ def ip ():
  var3=(resultado.group())
  return var3
 
+
 def dirb():
- os.system("pkg install dirb")
  banner()
  salto = "\n"
  print()
- print(f"{color.morado}    BUSCAR DIRECTORIOS {color.fin}")
+ print(f"{color.morado}    BUSCAR SUBDOMINIOS {color.fin}")
  print()
  print(f"{color.cyan}QUE QUIE TIPO DE SCANEO QUIERES{color.fin}")
  print()
@@ -277,14 +277,56 @@ def dirb():
  var=input(f"{color.cyan}ELIJE UN NUMERO >> {color.fin}")
  if var == "1":
   banner()
-  print(f"{color.amarillo}EJEMPLO HTTPS://WWW.GOOGLE.ES{color.fin}")
   print()
-  var1=input(f"{color.cyan}INTRODUCE LA PAGINA >> {color.fin}")
+  print("EJEMPLO : https://google.com")
+  url = input(f"{color.cyan}introduce pagina >> {color.fin}")
+  fd = open("small.txt","r")
+  leer = fd.readlines()
+  df =open("buenos.txt","w")
+  df.write("")
+  df.close()
+  df =open("buenos.txt","a")
+  contador = 0
+  otra =requests.get(url+"/!?g7")
+  largura1=otra.text
+  largura1 = len(largura1)-4
+  otra =requests.get(url+"/!8!7")
+  largura1=otra.text
+  largura1 = len(largura1)-14
+  largura2 = largura1+34
+  for linea in leer:
+   contador +=1
+   if contador == 200 :
+    print(f"{color.verde} PROBADAS 200 DE 1000 CONTRASEÑAS{color.fin}")
+   if contador == 400 :
+    print(f"{color.verde} PROBADAS 400 DE 1000 CONTRASEÑAS{color.fin}")
+   if contador == 600 :
+    print(f"{color.verde} PROBADAS 600 DE 1000 CONTRASEÑAS{color.fin}")
+   if contador == 800 :
+    print(f"{color.verde} PROBADAS 800 DE 1000 CONTRASEÑAS{color.fin}")
+   var = linea.rstrip("\n")
+   print(url+"/"+var)
+   data = url+"/"+var
+ 
+   try:
+    r= requests.get(data)
+    largura=r.text
+    largura = len(largura)
+    largura = largura - len(var)
+    if largura1 > largura or largura2 < largura:
+     print(f"{color.amarillo}")
+     print(data)
+     df.write((data) + f"{salto}")
+     print(f"{color.fin}")
+   except:
+    pass
+  df.close()
   banner()
+  print(f"{color.morado} RESULTADOS OPTENIDOS {color.amarillo}")
+  df = open("buenos.txt","r")
+  leer = df.read()
+  print(leer)
   print()
-  print(f"{color.amarillo}TEN PACIENCIA ESTO PUEDE TARDAR VEINTE MINUTOS{color.fin}")
-  time.sleep(3)
-  os.system(f"dirb {var1} small.txt -r")
   print(f"{color.morado}QUE QUIERES HACER AHORA{color.fin}")
   print()
   print(f"{color.azul}[1] VOLVER")
@@ -309,14 +351,55 @@ def dirb():
   var=input(f"{color.cyan}ELIJE UN NUMERO >> {color.fin}")
   if var =="1":
    banner()
-   print(f"{color.amarillo}EJEMPLO HTTPS://WWW.GOOGLE.ES{color.fin}")
    print()
-   var1=input(f"{color.cyan}INTRODUCE LA PAGINA >> {color.fin}")
+   print("EJEMPLO : https://google.com")
+   url = input(f"{color.cyan}introduce pagina >> {color.fin}")
+   fd = open("commun.txt","r")
+   leer = fd.readlines()
+   df =open("buenos.txt","w")
+   df.write("")
+   df.close()
+   df =open("buenos.txt","a")
+   contador = 0
+   otra =requests.get(url+"/!?g7")
+   largura1=otra.text
+   largura1 = len(largura1)-4
+   otra =requests.get(url+"/!8!7")
+   largura1=otra.text
+   largura1 = len(largura1)-14
+   largura2 = largura1+34
+   for linea in leer:
+    contador +=1
+    if contador == 2000 :
+     print(f"{color.verde} PROBADAS 2000 de 10000 CONTRASEÑAS{color.fin}")
+    if contador == 4000 :
+     print(f"{color.verde} PROBADAS 4000 de 10000 CONTRASEÑAS{color.fin}")
+    if contador == 6000 :
+     print(f"{color.verde} PROBADAS 6000 de 10000 CONTRASEÑAS{color.fin}")
+    if contador == 8000 :
+     print(f"{color.verde} PROBADAS 8000 de 10000  CONTRASEÑAS{color.fin}")
+    var = linea.rstrip("\n")
+    print(url+"/"+var)
+    data = url+"/"+var
+    try:
+     r= requests.get(data)
+     largura=r.text
+     largura = len(largura)
+     largura = largura - len(var)
+     if largura1 > largura or largura2 < largura:
+      print(f"{color.amarillo}")
+      print(data)
+      df.write((data) + f"{salto}")
+      print(f"{color.fin}")
+    except:
+     pass
+   df.close()
    banner()
+   print(f"{color.morado} RESULTADOS OPTENIDOS {color.amarillo}")
+   df = open("buenos.txt","r")
+   leer = df.read()
+   print(leer)
    print()
-   print(f"{color.amarillo}TEN PACIENCIA ESTO VA A TARDAR UN RATO{color.fin}")
-   time.sleep(3)
-   os.system(f"dirb {var1} common.txt -r")
    print(f"{color.morado}QUE QUIERES HACER AHORA{color.fin}")
    print()
    print(f"{color.azul}[1] VOLVER")
@@ -326,19 +409,19 @@ def dirb():
    if var == "1":
     menu()
    elif var == "0":
-     salir()
+    salir()
    else :
     incorrecto()
-  elif var=="2":
-   menu()
-  elif var =="0":
+  elif var =="2":
+   dirb()
+  elif var == "0":
    salir()
-  else:
+  else :
    incorrecto()
- elif var =="0":
+ elif var == "0":
   salir()
- else:
-  incorrecto()
+ else :
+   incorrecto()
 
 def nmap():
  os.system("pkg install nmap")
